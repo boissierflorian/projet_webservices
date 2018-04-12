76c3a78a806d23ac86647dbe42dc6af00df0031f
Boissier Florian <boissierflorian@outlook.com>
2018-04-12 13:51:51 +0200

    feat(config.json) : ajout de la configuration de la base de données

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// configuration 
+var fs = require('fs');
+var configData = fs.readFileSync('config.json', 'utf8');
+databaseConfig = JSON.parse(configData);
+
+require('./modules/modules');
+
 var app = express();
 
 // view engine setup
