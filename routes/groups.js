a18bf478cf52c68a0b5562b6842a838974598df0
hmainismael <hmain.ismael@hotmail.fr>
2018-04-13 15:23:13 +0200

    fix(routes): ajout de 2 routes pour la gestion des Group

--- routes/groups.js
+++ routes/groups.js
@@ -6,6 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('Display Groups');
 });
 
+router.get('/:id([0-9]+)', function(req, res, next) {
+  res.send('Afficher un seul groupe');
+});
+
 router.post('/', function(req, res, next) {
   res.send('Ajouter un groupe');
 });
@@ -16,6 +20,10 @@ router.put('/:id([0-9]+)', function(req, res, next) {
 
 router.delete('/:id([0-9]+)', function(req, res, next) {
   res.send('Supprimer un groupe');
+});
+
+router.delete('/:groupId([0-9]+)/user/:userId([0-9]+)', function(req, res, next) {
+  res.send('Supprimer un utilisateur d\'un groupe spécifié');
 });
 
 module.exports = router;
