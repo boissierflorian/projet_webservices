95d6d00fa79ad84d4b2bdec75420f06a880e5ebd
gmachure <gmachure@adexos.fr>
2018-05-14 10:15:48 +0200

    feat(service): add user to group method

--- services/usergroup.js
+++ services/usergroup.js
@@ -2,7 +2,7 @@ const models = require('../models');
 
 exports.getUsers = function(params) {
     return models.UserGroup.getUsers(params.id);
-}
+};
 
 exports.deleteUserFromGroup = function(params) {
     return models.UserGroup.destroy({
@@ -14,4 +14,21 @@ exports.deleteUserFromGroup = function(params) {
         console.log(e);
         return {error : e};
     });
-}
+};
+
+exports.addUserToGroup = function(params){
+    return models.UserGroup.create({
+        id_user: params.userId,
+        id_group: params.groupId
+    }).then(function(userGroup){
+        if(userGroup !== undefined){
+            userGroup.dataValues.created = true;
+            return userGroup.dataValues;
+        }else{
+            return {created: false}
+        }
+    }).catch(function (e) {
+        console.log(e);
+        return {error: e};
+    });
+};
