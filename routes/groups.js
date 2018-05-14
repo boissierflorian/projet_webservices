77895485ac690bdbf3183d546586293a4f89e15e
gmachure <gmachure@adexos.fr>
2018-05-14 09:49:09 +0200

    feat(routing): add route in order to add a user to a specific group

--- routes/groups.js
+++ routes/groups.js
@@ -3,12 +3,18 @@ var router = express.Router();
 var GroupService = require('../services/groups');
 var UserGroupService = require('../services/usergroup');
 
+/**
+ * List all groups
+ */
 router.get('/', function(req, res, next) {
   GroupService.getGroups().then(result => {
     res.json(result);
   });
 });
 
+/**
+ * Get a specific group
+ */
 router.get('/:id([0-9]+)', function(req, res, next) {
   GroupService.getGroup(req.params).then(result => {
     UserGroupService.getUsers(req.params).then(users => {
@@ -18,12 +24,18 @@ router.get('/:id([0-9]+)', function(req, res, next) {
   });
 });
 
+/**
+ * Create group
+ */
 router.post('/', function(req, res, next) {
     GroupService.createGroup(req.body).then(result => {
         res.json(result);
     });
 });
 
+/**
+ * Update specific group
+ */
 router.put('/:id([0-9]+)', function(req, res, next) {
   req.body.id = req.params.id;
 
@@ -32,12 +44,27 @@ router.put('/:id([0-9]+)', function(req, res, next) {
   });
 });
 
+/**
+ * Delete specific group
+ */
 router.delete('/:id([0-9]+)', function(req, res, next) {
   GroupService.deleteGroup(req.params).then(result => {
     res.json(result);
   });
 });
 
+/**
+ * Add User to a specific group
+ */
+router.post('/user/:id([0-9]+)', function(req, res, next) {
+   GroupService.addUserToGroup(req.params).then(result => {
+       res.json(result);
+   })
+});
+
+/**
+ * Delete User from a specific group
+ */
 router.delete('/:groupId([0-9]+)/user/:userId([0-9]+)', function(req, res, next) {
   UserGroupService.deleteUserFromGroup(req.params).then(result => {
     res.json(result);
